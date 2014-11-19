c94cbdd93dbe5bab9d07a3cf00a0bab1f5972ee3
a00880208 <alam160@my.bcit.ca>
2014-11-19 10:29:40 -0800

    pasted psuedocode

--- src/main.cpp
+++ src/main.cpp
@@ -3,3 +3,36 @@ using namespace std;
 //Georgi says hello
 //TESTING AS SEBATIAN
 //HALLOOO
+
+/*
+//WinMain method
+setup window class
+create the window and store objects in the window
+display the window on the screen
+initialize input / output buffers
+start the listening thread
+
+if you generate a send message event
+begin a transmitting thread
+
+//WndProc
+
+Go into a switch statement to handle messages
+
+WM Paint
+Paint GUI elements
+Paint all the texts from the overall buffer
+
+Menu item clicks
+Connect case:
+	Attempt to open a connection via RS - 232 with the modem
+		and the wireless network
+		Disconnect Case :
+	Close connection with the modem and the wireless network
+		Exit Program Case :
+	Close connection with the modem and terminate the program
+
+		User Events
+		Enter key pressed :
+	Put all characters into the transmission buffer and open the transmission thread.
+*/
