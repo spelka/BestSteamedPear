fdfeafec4bd77b28fe60acf85838103625e5d2e2
Melvin Loho <mloho@outlook.com>
2014-11-24 09:50:48 -0800

    updated protocol.h

--- src/protocol.h
+++ src/protocol.h
@@ -1,9 +1,17 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
+#include <vector>
+
 const char ENQ = 0x05;
 const char ACK = 0x06;
 const char RVI = 0x17;
 const char NAK = 0x21;
+
+struct WConn
+{
+	std::vector<char> buffer_receive;
+	std::vector<char> buffer_send;
+};
 
 #endif // PROTOCOL_H
