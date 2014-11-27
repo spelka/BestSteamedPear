72c127ab7d7bac55ab1387ca55951940564d443c
spelka <sebastian.pelka@hotmail.com>
2014-11-26 19:14:08 -0800

    wrote two functions for receive
    wrote two funtions for receiving characters

--- src/receive.cpp
+++ src/receive.cpp
@@ -44,3 +44,82 @@ stopWaiting = true
 if !ackReceived
 Sleep(TO3 + a randomly generated number)
 */
+
+
+#include "receive.h"
+#include "protocol.h"
+
+//receieve.cpp
+bool receivingMode = false;
+
+//receive an ENQ or RVI
+//send ACK
+//go into a receiving loop
+//if you timeout
+timeout += TO1
+//if timeout == TO3 * MAX_MISS
+//return to idle protocol
+//if you receive a packet
+//CRC validate the packet
+//if the packet is valid and ETB
+//return to receiving loop
+//else if the packet is invalid and miss < MAX_MISS
+//return to receiving loop
+//else if the packet is valid and EOT
+//print message to screen
+//return to idle protocol
+//else if you need to send
+//send RVI
+//on ACK of RVI, go to transmission thread
+
+
+void SendACK(HANDLE hComm)
+{
+	//Send an ACK
+	if (!WriteFile(hComm, ACK, 1, &bytesWritten, NULL))
+	{
+		MessageBox(NULL, TEXT("Error: Failed to send ACK"), TEXT("Error"), MB_OK | MB_ICONERROR);
+	}
+
+	return;
+}
+
+//receives a character from the comm port and checks if the received char is what was expected
+bool ReceiveChar(char expectedChar)
+{
+	char received;
+
+	//read in character from comm port
+	//if you fail to read in a file, return false
+	if (!ReadFile(GetWConn().hComm, &received, 1, NULL, NULL))
+	{
+		return false;
+	}
+	//if read was successful, compare the read in character to the expected character
+	if (expectedChar == received)
+	{
+		return true;
+	}
+	return false;
+}
+
+//receives a character from the comm port and returns it
+char ReceiveChar()
+{
+	char received;
+
+	//read in character from comm port
+	//if you fail to read in a file, return false
+	if (!ReadFile(GetWConn().hComm, &received, 1, NULL, NULL))
+	{
+		return 0x0;
+	}
+	//if read was successful, return the read-in character
+	return received;
+}
+
+
+void receivePacket()
+{
+
+}
