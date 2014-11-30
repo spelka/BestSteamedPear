ec7b13967717b2a2a0c4f150d032961d9402626f
spelka <sebastian.pelka@hotmail.com>
2014-11-29 17:50:49 -0800

    protocol.h
    added EOT, ETB, changed vectors to deques

--- src/protocol.h
+++ src/protocol.h
@@ -2,14 +2,16 @@
 #define PROTOCOL_H
 
 #include <windows.h>
-#include <vector>
+#include <deque>
 
 const char NUL = 0x00;
 const char ETX = 0x03;
+const char EOT = 0x04;
 const char ENQ = 0x05;
 const char ACK = 0x06;
 const char RVI = 0x17;
 const char NAK = 0x21;
+const char ETB = 0x23;
 const char PAD = 0x54;
 
 const unsigned PACKET_SIZE = 1024; //packet size in bytes
@@ -28,8 +30,8 @@ struct WConn
 		TO4
 		;
 
-	std::vector<char> buffer_receive;
-	std::vector<char> buffer_send;
+	std::deque<char> buffer_receive;
+	std::deque<char> buffer_send;
 };
 
 WConn& GetWConn();
